62424a5bce767340f450c5e50e5798ccc7527d22
ssu95a <ssu95a@gmail.com>
2026-06-10 21:28:30 +0300

    Carry page orientation in render state

--- src/main/java/ru/inversion/f2/style/F2RenderState.java
+++ src/main/java/ru/inversion/f2/style/F2RenderState.java
@@ -1,5 +1,7 @@
 package ru.inversion.f2.style;
 
+import javax.print.attribute.standard.OrientationRequested;
+
 /** */
 public final class F2RenderState {
 
@@ -7,20 +9,36 @@ public final class F2RenderState {
 
     private final double lineStepPt;
     private final double leftIndentPt;
+    private final OrientationRequested orientation;
 
     public F2RenderState(
         F2Style style,
         double lineStepPt,
         double leftIndentPt
+    ) {
+        this(
+                style,
+                lineStepPt,
+                leftIndentPt,
+                OrientationRequested.PORTRAIT
+        );
+    }
+
+    public F2RenderState(
+        F2Style style,
+        double lineStepPt,
+        double leftIndentPt,
+        OrientationRequested orientation
     )
     {
         this.style        = style == null ? F2Style.defaultStyle() : style;
         this.lineStepPt   = lineStepPt;
         this.leftIndentPt = leftIndentPt;
+        this.orientation  = orientation == null ? OrientationRequested.PORTRAIT : orientation;
     }
 
     public static F2RenderState initial ( ) {
-        return new F2RenderState(F2Style.defaultStyle(), 12.0d, 0.0d);
+        return new F2RenderState(F2Style.defaultStyle(), 12.0d, 0.0d, OrientationRequested.PORTRAIT);
     }
 
     public F2Style style() {
@@ -28,15 +46,19 @@ public final class F2RenderState {
     }
 
     public F2RenderState withStyle(F2Style value) {
-        return new F2RenderState(value, lineStepPt, leftIndentPt);
+        return new F2RenderState(value, lineStepPt, leftIndentPt, orientation);
     }
 
     public F2RenderState withLineStepPt(double value) {
-        return new F2RenderState(style, value, leftIndentPt);
+        return new F2RenderState(style, value, leftIndentPt, orientation);
     }
 
     public F2RenderState withLeftIndentPt(double value) {
-        return new F2RenderState(style, lineStepPt, value);
+        return new F2RenderState(style, lineStepPt, value, orientation);
+    }
+
+    public F2RenderState withOrientation(OrientationRequested value) {
+        return new F2RenderState(style, lineStepPt, leftIndentPt, value);
     }
 
     public double lineStepPt() {
@@ -45,5 +67,9 @@ public final class F2RenderState {
 
     public double leftIndentPt() {
         return leftIndentPt;
+    }
+
+    public OrientationRequested orientation() {
+        return orientation;
     }
 }
