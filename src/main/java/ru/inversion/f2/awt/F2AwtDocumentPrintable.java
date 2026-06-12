5c369e627c78fc58f44bc780eccdaabbeefd35a4
ssu95a <ssu95a@gmail.com>
2026-06-13 00:22:52 +0300

    Clean document printable debug state

--- src/main/java/ru/inversion/f2/awt/F2AwtDocumentPrintable.java
+++ src/main/java/ru/inversion/f2/awt/F2AwtDocumentPrintable.java
@@ -2,8 +2,6 @@ package ru.inversion.f2.awt;
 
 import ru.inversion.f2.prepared.F2StyledDocument;
 import ru.inversion.f2.prepared.F2StyledPage;
-import ru.inversion.f2.print.F2PrintPageSetup;
-import ru.inversion.utils.Checks;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -15,31 +13,37 @@ public final class F2AwtDocumentPrintable implements Printable {
 
     private final F2StyledDocument document;
     private final F2AwtPagePainter painter;
-    private final F2PrintPageSetup pageSetup;
 
     public F2AwtDocumentPrintable(
-        F2StyledDocument document,
-        F2PrintPageSetup pageSetup
-    )
-    {
-        this(document, pageSetup, new F2AwtPagePainter());
+            F2StyledDocument document
+    ) {
+        this(
+                document,
+                new F2AwtPagePainter()
+        );
     }
 
     /** */
     public F2AwtDocumentPrintable(
             F2StyledDocument document,
-            F2PrintPageSetup pageSetup,
             F2AwtPagePainter painter
-    )
-    {
-        this.document  = Checks.Require.object(document, "document");
-        this.pageSetup = Checks.Require.object(pageSetup, "pageSetup");
-        this.painter   = Checks.Require.object(painter, "painter");
+    ) {
+        if (document == null)
+            throw new IllegalArgumentException("document is null");
+
+        if (painter == null)
+            throw new IllegalArgumentException("painter is null");
+
+        this.document = document;
+        this.painter = painter;
     }
 
     @Override
     public int print( Graphics graphics, PageFormat pageFormat, int pageIndex )
     {
+        if (pageIndex < 0 || pageIndex >= document.pageCount())
+            return NO_SUCH_PAGE;
+
         F2StyledPage page = document.pages().get(pageIndex);
 
         F2AwtPageRenderConfig config =
@@ -48,18 +52,6 @@ public final class F2AwtDocumentPrintable implements Printable {
                         72.0d,
                         false
                 ).withShrinkToFit(true);
-
-        System.out.println(
-                "PRINT pageIndex=" + pageIndex
-                        + ", pageLandscape=" + page.isLandscape()
-                        + ", pfOrientation=" + pageFormat.getOrientation()
-                        + ", pf=[" + pageFormat.getWidth() + "x" + pageFormat.getHeight() + "]"
-                        + ", imageable=[" + pageFormat.getImageableX()
-                        + "," + pageFormat.getImageableY()
-                        + "," + pageFormat.getImageableWidth()
-                        + "," + pageFormat.getImageableHeight()
-                        + "]"
-        );
 
         painter.paint((Graphics2D) graphics, page, config);
 
